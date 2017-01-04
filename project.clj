533988dedb0a089e7e2bcce5c812e92a088c58f9
Phoenix Zhang <pho.coderzhang@gmail.com>
2017-01-04 18:32:04 +0800

    fix

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject rocks.pho.btc/whac-a-mole "0.1.7.1-SNAPSHOT"
+(defproject rocks.pho.btc/whac-a-mole "0.1.8.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
